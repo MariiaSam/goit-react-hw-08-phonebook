9b7cc6dd1b4986d94bbb4408b0cdbb780fa1f7e0
MariiaSam <kontragent@ukr.net>
2024-01-19 16:13:40 +0200

    add Forms

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,21 @@
+
+
+const LoginForm = () => {
+
+    return (
+        <form>
+        
+            <label htmlFor="">Email
+                <input type="text" name="email" />
+            </label>
+            <label htmlFor="">Password
+                <input type="text" name="password" />
+            </label>
+            <button>Log in</button>
+        </form>
+        
+        )
+    
+    }
+    
+    export default LoginForm
---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,23 @@
+
+
+const RegisterForm = () => {
+
+return (
+    <form>
+        <label htmlFor="">Username
+            <input type="text" name="username" id="" />
+        </label>
+        <label htmlFor="">Email
+            <input type="text" name="email" id="" />
+        </label>
+        <label htmlFor="">Password
+            <input type="text" name="password" id="" />
+        </label>
+        <button>Sing up</button>
+    </form>
+    
+    )
+
+}
+
+export default RegisterForm
